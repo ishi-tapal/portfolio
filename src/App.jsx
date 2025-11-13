27da8c696c5baa307623697a04c1bda2c86dbfad
ishi-tapal <310ishitapal@gmail.com>
2025-11-13 17:26:54 +0530

    Styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,6 @@ import {
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
-import image from "./assets/rough-black-lines-textured-background.jpg";
 import ParticleBackground from "./Particle";
 import Typewriter from "typewriter-effect";
 //import Typed from "react-typed";
